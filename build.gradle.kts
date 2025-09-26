f31e6ac75fec1d10d98a0e6ef14f08c99e66596e
Lucas Plumb <virual@gmail.com>
2025-09-26 17:42:48 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,4 @@ plugins {
     alias(libs.plugins.androidxNavigationSafeArgs) apply false
     alias(libs.plugins.androidxRoom) apply false
 
-    // PlayStore version only
-    alias(libs.plugins.googleCrashlytics) apply false
-    alias(libs.plugins.googleGms) apply false
 }
