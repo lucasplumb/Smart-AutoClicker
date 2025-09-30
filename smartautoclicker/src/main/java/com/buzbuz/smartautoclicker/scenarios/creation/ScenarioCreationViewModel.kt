399643881ac2598868b92d4a1977f8149226b15e
Lucas Plumb <virual@gmail.com>
2025-09-30 15:36:47 -0400

    Update ScenarioCreationViewModel.kt

--- smartautoclicker/src/main/java/com/buzbuz/smartautoclicker/scenarios/creation/ScenarioCreationViewModel.kt
+++ smartautoclicker/src/main/java/com/buzbuz/smartautoclicker/scenarios/creation/ScenarioCreationViewModel.kt
@@ -63,7 +63,8 @@ class ScenarioCreationViewModel @Inject constructor(
         .take(1)
     val nameError: Flow<Boolean> = _name
         .map { it.isNullOrEmpty() }
-private val bval: Flow<Boolean> = flow{ false }
+private val bval: Flow<Boolean> = _name
+        .map { it.isNullOrEmpty() }
     private val _selectedType: MutableStateFlow<ScenarioTypeSelection> =
         MutableStateFlow(ScenarioTypeSelection.SMART)
     val scenarioTypeSelectionState: Flow<ScenarioTypeSelectionState> =  combine(_selectedType, bval){ selectedType, bvalue ->
